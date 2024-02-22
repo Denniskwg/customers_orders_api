ab9b9926cc7f118b50ddc4614170b01194822974
Denniskwg <dkamau476@gmail.com>
2024-02-22 12:26:14 +0300

    adding logging

--- api/tasks.py
+++ api/tasks.py
@@ -1,8 +1,15 @@
 from .sms import SMS
 from celery import shared_task
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 @shared_task
 def send_sms_notification(recepients, item):
     """Sends a message when an order has been submitted."""
-    SMS().send(recepients, item)
+    try:
+        SMS().send(recepients, item)
+        logger.info("Task executed successfully")
+    except Exception as e:
+        logger.exception("An error occurred in the task: %s", e)
