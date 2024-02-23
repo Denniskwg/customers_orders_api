851a93fc5b43a46e177067b58c029c46a4b86320
Denniskwg <dkamau476@gmail.com>
2024-02-23 13:48:51 +0300

    corrections

--- api/settings.py
+++ api/settings.py
@@ -20,7 +20,10 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 db_from_env = dj_database_url.config()
 
 STATIC_ROOT = 'staticfiles'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = { 
+    os.path.join(BASE_DIR, 'static')
+}
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
