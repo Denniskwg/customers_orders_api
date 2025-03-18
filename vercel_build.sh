7d72cc2b3ec35ccf1a20d4d48d79770fdcb897bd
root <root@DESKTOP-62R1M4Q.localdomain>
2025-03-18 13:54:05 +0300

    Corrections to vercel config

--- vercel_build.sh
+++ vercel_build.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+python3 pip install --upgrade pip
+python3 pip install --no-cache-dir -r requirements.txt
 
-python3 -m pip install -r requirements.txt
+echo "🔹 Checking Gunicorn installation..."
+which gunicorn || echo "⚠️ Gunicorn NOT found in PATH!"
+pip show gunicorn || echo "⚠️ Gunicorn NOT installed in this environment!"
+
 python3 manage.py migrate
 python3 manage.py collectstatic --noinput
 export PATH="$HOME/.local/bin:$PATH"
+
+echo "🔹 Starting Gunicorn..."
 gunicorn api.wsgi:application --bind 0.0.0.0:8000
