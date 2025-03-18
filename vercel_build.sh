19edbd4074bb4348fae2c77d24362bba1159bed4
root <root@DESKTOP-62R1M4Q.localdomain>
2025-03-18 14:23:53 +0300

    fixing typos

--- vercel_build.sh
+++ vercel_build.sh
@@ -2,7 +2,9 @@
 python3 -m ensurepip --default-pip || curl https://bootstrap.pypa.io/get-pip.py | python3
 python3 -m pip install --upgrade pip setuptools wheel
 
-python3 pip install --no-cache-dir -r requirements.txt
+export PATH="/python312/bin:$PATH"
+
+python3 -m pip install --no-cache-dir -r requirements.txt
 echo "🔹 Checking Gunicorn installation..."
 which gunicorn || echo "⚠️ Gunicorn NOT found in PATH!"
 pip show gunicorn || echo "⚠️ Gunicorn NOT installed in this environment!"
