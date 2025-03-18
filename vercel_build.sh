9bc79f7a44508dfeafc22a27a47d408604afa374
root <root@DESKTOP-62R1M4Q.localdomain>
2025-03-18 13:44:09 +0300

    Corrections

--- vercel_build.sh
+++ vercel_build.sh
@@ -3,4 +3,5 @@
 python3 -m pip install -r requirements.txt
 python3 manage.py migrate
 python3 manage.py collectstatic --noinput
+export PATH="$HOME/.local/bin:$PATH"
 gunicorn api.wsgi:application --bind 0.0.0.0:8000
